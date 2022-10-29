751cbe66bef12f74c4ed45d0e2a2f44b1d241b63
Italo Covas <italocovas@gmail.com>
2022-10-29 17:43:21 -0300

    Database connection - User Entity and Migration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
+import 'express-async-errors';
 import express from 'express';
 import { AppDataSource } from './data-source';
+import { errorMiddleware } from './middlewares/error';
+import routes from './routes';
 
 AppDataSource.initialize().then(() => {
     const app = express();
 
     app.use(express.json());
 
-    app.get('/', (req, res) => {
-        return res.json('tudo certo');
-    });
+    app.use(routes);
 
+    // Always use error middlewares before app.listen() => after all other middlewares
+    app.use(errorMiddleware);
     return app.listen(process.env.PORT);
 });
